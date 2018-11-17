5dff4de10a5fdbbb4a1e872d15b444b40813421c
Aftab Alam <aftab.a0292@gmail.com>
2018-11-17 12:52:36 +0530

    Made changed in middleware for under maintainance

--- server.js
+++ server.js
@@ -5,15 +5,16 @@ const fs = require('fs');
 
 hbs.registerPartials(__dirname + '/views/partials')
 app.set('view engine', 'hbs');
-app.use(express.static(__dirname + '/public'));
+
 hbs.registerHelper('getCurrentYear', ()=>{
-    return new Date().getFullYear()
+    return new Date().getFullYear();
 });
 
 hbs.registerHelper('screamIt', (text)=>{
     return text.toUpperCase();
 });
 
+// Server logs
 app.use((req, res, next)=>{
     let now = new Date().toString();
     let log = `${now} : ${req.method} : ${req.url}`
@@ -22,10 +23,15 @@ app.use((req, res, next)=>{
     next();
 });
 
-app.use((req, res)=>{
+/* app.use((req, res)=>{
     res.render('maintenance.hbs')
 });
+ */
 
+// Serve static directory
+app.use(express.static(__dirname + '/public'));
+
+// home page routes
 app.get('/', (req, res) => {
     res.render('home.hbs', {
         pageTitle: 'Home Page',
@@ -33,18 +39,23 @@ app.get('/', (req, res) => {
     });
 });
 
+// about page routes
 app.get('/about', (req, res) => {
     res.render('about.hbs', {
         pageTitle: 'About Page'
-    })
-})
+    });
+});
 
+// Bad page routes
 app.get('/bad', (req, res) => {
     res.send({
         errorMessage: 'Unable to handle error'
     });
-})
+});
 
+// Node Server
 app.listen(3000, () => {
     console.log(`Server is listening on PORT: 3000`);
 });
+
+// https://www.intel.com/content/www/us/en/support/intel-driver-support-assistant.html
